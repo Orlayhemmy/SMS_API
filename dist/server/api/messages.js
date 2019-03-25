c15225e7ed02b6bbe978fcef63e3e721ed9c92c3
Orlayhemmy <daminomics@gmail.com>
2019-03-25 19:46:27 +0100

    update dist folder

--- dist/server/api/messages.js
+++ dist/server/api/messages.js
@@ -48,27 +48,25 @@ var sendMessage = exports.sendMessage = function () {
             isReceiver = _context.sent;
 
             if (!(isSender.length && isReceiver.length)) {
-              _context.next = 16;
+              _context.next = 14;
               break;
             }
 
             new_message.receiver = receiver;
             new_message.sender = sender;
             new_message.msg = msg;
-            new_message.senderStatus = true;
-            new_message.receiverStatus = true;
             new_message.save(function (err, result) {
               if (err) return res.status(500).send({ message: 'Message not sent' });
               return res.status(201).send({ status: 'message sent successfully', message: result.msg });
             });
             return _context.abrupt('return');
 
-          case 16:
+          case 14:
             return _context.abrupt('return', res.status(400).send({
               message: 'Receiver or Sender phone number is invalid'
             }));
 
-          case 17:
+          case 15:
           case 'end':
             return _context.stop();
         }
@@ -81,24 +79,70 @@ var sendMessage = exports.sendMessage = function () {
   };
 }();
 
-var getSentMessages = exports.getSentMessages = function getSentMessages(_ref3, res) {
-  var id = _ref3.params.id;
+var getSentMessages = exports.getSentMessages = function () {
+  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(_ref3, res) {
+    var id = _ref3.params.id;
+    var phone;
+    return _regenerator2.default.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return _contacts2.default.findOne({ _id: id });
 
-  _message2.default.find({ sender: id }, function (err, result) {
-    if (err) return res.status(500).send({ message: 'Network error' });
-    return res.status(200).send({
-      messages: result
-    });
-  });
-};
+          case 2:
+            phone = _context2.sent;
 
-var getInboxes = exports.getInboxes = function getInboxes(_ref4, res) {
-  var id = _ref4.params.id;
+            _message2.default.find({ sender: phone.phone_num }, function (err, result) {
+              if (err) return res.status(500).send({ message: 'Network error' });
+              return res.status(200).send({
+                messages: result
+              });
+            });
 
-  _message2.default.find({ receiver: id }, function (err, result) {
-    if (err) return res.status(500).send({ message: 'Network error' });
-    return res.status(200).send({
-      messages: result
-    });
-  });
-};
+          case 4:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  }));
+
+  return function getSentMessages(_x3, _x4) {
+    return _ref4.apply(this, arguments);
+  };
+}();
+
+var getInboxes = exports.getInboxes = function () {
+  var _ref6 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(_ref5, res) {
+    var id = _ref5.params.id;
+    var phone;
+    return _regenerator2.default.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            _context3.next = 2;
+            return _contacts2.default.findOne({ _id: id });
+
+          case 2:
+            phone = _context3.sent;
+
+            _message2.default.find({ receiver: phone.phone_num }, function (err, result) {
+              if (err) return res.status(500).send({ message: 'Network error' });
+              return res.status(200).send({
+                messages: result
+              });
+            });
+
+          case 4:
+          case 'end':
+            return _context3.stop();
+        }
+      }
+    }, _callee3, undefined);
+  }));
+
+  return function getInboxes(_x5, _x6) {
+    return _ref6.apply(this, arguments);
+  };
+}();
--- dist/server/lib/message.js
+++ dist/server/lib/message.js
@@ -9,9 +9,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var messageSchema = new _mongoose2.default.Schema({
   msg: { type: String },
   sender: { type: String },
-  receiver: { type: String },
-  senderStatus: { type: Boolean },
-  receiverStatus: { type: Boolean }
+  receiver: { type: String }
 });
 
 var Message = _mongoose2.default.model('message', messageSchema);
