d3ef768e33319bdcb8da7b9896c0ea6e558b654e
Orlayhemmy <daminomics@gmail.com>
2019-03-25 09:58:39 +0100

    refresh dist folder

--- dist/server/server.js
+++ dist/server/server.js
@@ -12,9 +12,7 @@ var _dotenv = require('dotenv');
 
 var _dotenv2 = _interopRequireDefault(_dotenv);
 
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _dotenv2.default.config();
 
--- server/server.js
+++ server/server.js
@@ -1,37 +1,25 @@
-'use strict';
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-var _express = require('express');
+dotenv.config();
 
-var _express2 = _interopRequireDefault(_express);
-
-var _mongoose = require('mongoose');
-
-var _mongoose2 = _interopRequireDefault(_mongoose);
-
-var _dotenv = require('dotenv');
-
-var _dotenv2 = _interopRequireDefault(_dotenv);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_dotenv2.default.config();
-
-var app = (0, _express2.default)();
-var port = process.env.PORT || 6000;
+const app = express();
+const port = process.env.PORT || 6000;
 
 // Set up mongoose connection
-var mongoDB = process.env.MONGODB_URI;
-_mongoose2.default.connect(mongoDB, { useNewUrlParser: true });
-_mongoose2.default.Promise = global.Promise;
-var db = _mongoose2.default.connection;
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(_express2.default.json());
-app.use(_express2.default.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port);
-console.log('App running on ' + port);
+console.log(`App running on ${port}`);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Welcome to our SMS API');
-});
+})
