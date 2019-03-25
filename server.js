2a59bf89c5e77c8c2a811b28735d796d58058ed0
Orlayhemmy <daminomics@gmail.com>
2019-03-25 09:15:45 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 6000;
+
+// Set up mongoose connection
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port);
+console.log(`App running on ${port}`);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our SMS API');
+})
